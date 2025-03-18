5a8788a7886b3bb5ac2528d7781de770714f97fc
bevane <bevane@live.com>
2025-03-18 14:27:52 +0700

    feat: add meilisearch client

--- main.go
+++ main.go
@@ -7,13 +7,23 @@ import (
 
 	"github.com/a-h/templ"
 	"github.com/bevane/safina-society-search/views"
+	"github.com/meilisearch/meilisearch-go"
 )
 
+type Config struct {
+	searchClient *meilisearch.ServiceManager
+	port         int
+}
+
 func main() {
-	port := 3000
+	app := Config{}
+	app.port = 3000
+	searchClient := meilisearch.New("http://localhost:7700", meilisearch.WithAPIKey("masterKey"))
+	app.searchClient = &searchClient
+
 	publicHandler := http.StripPrefix("/public", http.FileServer(http.Dir("./public")))
 	http.Handle("/", templ.Handler(views.Index()))
 	http.Handle("/public/", publicHandler)
-	fmt.Printf("Server started on port %v\n", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+	fmt.Printf("Server started on port %v\n", app.port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", app.port), nil))
 }
