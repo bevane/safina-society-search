b73739078619f97b3fac827e17ad6a711d1c766b
bevane <bevane@live.com>
2025-03-20 22:37:02 +0700

    fix: calculate timestamp of cropped transcript (snippet)

--- handlers.go
+++ handlers.go
@@ -37,12 +37,18 @@ func (cfg *Config) handlerSearch(w http.ResponseWriter, r *http.Request) {
 		Items: make([]model.Result, len(searchResponse.Hits)),
 	}
 	for i, hit := range searchResponse.Hits {
-		timestampSeconds, err := getTimestampSecondsAtPosition(hit.Transcript, hit.MatchesPosition.Transcript[0].Start)
+		// look for the timestamp from the middle of the cropped transcript
+		// as the crop is centered around the word
+		// it is still possible to miss the timestamp since the search term
+		// is centered in regards to number of words instead of number of
+		// chars
+		timestampSeconds, err := getTimestampSecondsAtPosition(hit.Formatted.Transcript, len(hit.Formatted.Transcript)/2)
 		if err != nil {
 			fmt.Println(err)
 		}
 		results.Items[i] = model.Result{
-			Title:        hit.Formatted.Title,
+			Title: hit.Formatted.Title,
+			// construct url linking to timestamp of the crop/snippet
 			Url:          fmt.Sprintf("https://youtu.be/%s&t=%s", hit.Id, timestampSeconds),
 			ThumbnailUrl: hit.ThumbnailUrl,
 			Snippet:      hit.Formatted.Transcript,
@@ -53,23 +59,23 @@ func (cfg *Config) handlerSearch(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func getTimestampSecondsAtPosition(transcript string, position int) (string, error) {
+func getTimestampSecondsAtPosition(text string, position int) (string, error) {
 	var timestampStr string
 	r, _ := regexp.Compile("((?:\\d?\\d?:)?\\d?\\d?:\\d?\\d?)")
 	for position >= 0 {
 		// avoid looking for timestamp if position is not at a space
 		// or beginning of transcript to prevent capturing partial
 		// matches of timestamp
-		if position != 0 && string(transcript[position]) != " " {
+		if position != 0 && string(text[position]) != " " {
 			position--
 			continue
 		}
 		// keep looking if timestamp is not found within the next 8 chars
-		if !r.MatchString(transcript[position : position+9]) {
+		if !r.MatchString(text[position : position+9]) {
 			position--
 			continue
 		}
-		timestampStr = r.FindString(transcript[position : position+9])
+		timestampStr = r.FindString(text[position : position+9])
 		break
 	}
 	if timestampStr == "" {
@@ -95,7 +101,8 @@ func getTimestampSecondsAtPosition(transcript string, position int) (string, err
 	reference, _ := time.Parse(time.TimeOnly, "00:00:00")
 	timestampSeconds := timestamp.Sub(reference).Seconds()
 	// offset by 2 seconds to avoid cases where the timestamp will start
-	// right at the mention of the search query
+	// right at the mention of the search query making it likely for the
+	// user to miss the search query when watching the video
 	timestampSeconds = max(timestampSeconds-2, 0)
 	return strconv.Itoa(int(timestampSeconds)), nil
 }
